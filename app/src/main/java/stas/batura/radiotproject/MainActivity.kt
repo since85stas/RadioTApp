ba02aa31b7295b00012813f5080265f93b5b6c82
Note <seeyou85@gmail.com>
2022-10-04 00:11:57 +0300

    добавлены иконки

--- app/src/main/java/stas/batura/radiotproject/MainActivity.kt
+++ app/src/main/java/stas/batura/radiotproject/MainActivity.kt
@@ -21,23 +21,14 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.bumptech.glide.Glide
 import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.offline.DownloadHelper
-import com.google.android.exoplayer2.offline.DownloadRequest
-import com.google.android.exoplayer2.offline.DownloadService
 import com.google.android.exoplayer2.util.Util
 import com.google.android.material.navigation.NavigationView
-import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.nav_header_main.view.*
-import stas.batura.radioproject.data.ListViewType
-import stas.batura.radioproject.data.dataUtils.Year
-import stas.batura.radioproject.download.PodcastDownloadService
-import stas.batura.radioproject.musicservice.MusicService
-import stas.batura.radioproject.utils.CircleTransform
+import stas.batura.player.MusicService
 
 private lateinit var appBarConfiguration: AppBarConfiguration
 
-@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val TAG = MainActivity::class.java.simpleName
@@ -138,22 +129,22 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun testDownload() {
-//        val downloadRequest: DownloadRequest = DownloadRequest.Builder(
-//            "test3",
-//            Uri.parse("http://cdn.radio-t.com/rt_podcast669.mp3")
-//        ).build()
-        val mediaItem = MediaItem.fromUri(Uri.parse("http://cdn.radio-t.com/rt_podcast669.mp3"))
-        val helper = DownloadHelper.forMediaItem(
-            applicationContext,
-            mediaItem)
-
-        val request = helper.getDownloadRequest(Util.getUtf8Bytes(mediaItem.mediaId))
-        DownloadService.sendAddDownload(
-            this,
-            PodcastDownloadService::class.java,
-            request,
-            /* foreground= */ false
-        )
+////        val downloadRequest: DownloadRequest = DownloadRequest.Builder(
+////            "test3",
+////            Uri.parse("http://cdn.radio-t.com/rt_podcast669.mp3")
+////        ).build()
+//        val mediaItem = MediaItem.fromUri(Uri.parse("http://cdn.radio-t.com/rt_podcast669.mp3"))
+//        val helper = DownloadHelper.forMediaItem(
+//            applicationContext,
+//            mediaItem)
+//
+//        val request = helper.getDownloadRequest(Util.getUtf8Bytes(mediaItem.mediaId))
+//        DownloadService.sendAddDownload(
+//            this,
+//            PodcastDownloadService::class.java,
+//            request,
+//            /* foreground= */ false
+//        )
     }
 
 
