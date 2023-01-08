775489e4cda0ac585a63364ce3a197e7ba778727
Note <seeyou85@gmail.com>
2023-01-08 23:16:47 +0300

    добавлено больше тестов

--- timer/src/test/java/stas/batura/timer/TimerTest.kt
+++ timer/src/test/java/stas/batura/timer/TimerTest.kt
@@ -1,5 +1,6 @@
 package stas.batura.timer
 
+import org.junit.Assert
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -22,12 +23,66 @@ class TimerTest {
         goalTime.set(Calendar.HOUR_OF_DAY, 23)
         goalTime.set(Calendar.MINUTE, 0)
         goalTime.set(Calendar.SECOND, 0)
+        goalTime.set(Calendar.MILLISECOND, 0)
     }
 
     @Test
-    fun addition_isCorrect() {
+    fun check_hour_sec() {
         val timer = TimerImpl(goalTime)
-        
-        timer.getTimeDifference(Calendar.getInstance())
+
+        val calt = Calendar.getInstance()
+        calt.set(Calendar.DAY_OF_WEEK, Calendar.SATURDAY )
+        calt.set(Calendar.HOUR_OF_DAY, 22)
+        calt.set(Calendar.MINUTE, 0)
+        calt.set(Calendar.SECOND, 0)
+        calt.set(Calendar.MILLISECOND, 0)
+
+        val d = timer.getTimeDifference(calt, goalTime)
+        Assert.assertEquals(1*60*60, d)
+    }
+
+    @Test
+    fun check_hour_min() {
+        val timer = TimerImpl(goalTime, TimerImpl.DurationType.MINUTES)
+
+        val calt = Calendar.getInstance()
+        calt.set(Calendar.DAY_OF_WEEK, Calendar.SATURDAY )
+        calt.set(Calendar.HOUR_OF_DAY, 22)
+        calt.set(Calendar.MINUTE, 0)
+        calt.set(Calendar.SECOND, 0)
+        calt.set(Calendar.MILLISECOND, 0)
+
+        val d = timer.getTimeDifference(calt, goalTime)
+        Assert.assertEquals(1*60, d)
+    }
+
+    @Test
+    fun check_day_min() {
+        val timer = TimerImpl(goalTime, TimerImpl.DurationType.MINUTES)
+
+        val calt = Calendar.getInstance()
+        calt.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY )
+        calt.set(Calendar.HOUR_OF_DAY, 23)
+        calt.set(Calendar.MINUTE, 0)
+        calt.set(Calendar.SECOND, 0)
+        calt.set(Calendar.MILLISECOND, 0)
+
+        val d = timer.getTimeDifference(calt, goalTime)
+        Assert.assertEquals(1*60*24, d)
+    }
+
+    @Test
+    fun check_6day_min() {
+        val timer = TimerImpl(goalTime, TimerImpl.DurationType.MINUTES)
+
+        val calt = Calendar.getInstance()
+        calt.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY )
+        calt.set(Calendar.HOUR_OF_DAY, 23)
+        calt.set(Calendar.MINUTE, 0)
+        calt.set(Calendar.SECOND, 0)
+        calt.set(Calendar.MILLISECOND, 0)
+
+        val d = timer.getTimeDifference(calt, goalTime)
+        Assert.assertEquals(1*60*24*6, d)
     }
 }
