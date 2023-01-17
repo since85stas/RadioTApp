0fc43f5928a228f4a8bd44d3528b777f5745a1d5
Note <seeyou85@gmail.com>
2023-01-18 00:09:42 +0300

    настроены зависимости

--- analitics/src/main/java/stas/batura/analitics/AnaliticManagerImpl.kt
+++ analitics/src/main/java/stas/batura/analitics/AnaliticManagerImpl.kt
@@ -2,7 +2,6 @@ package stas.batura.analitics
 
 import android.content.Context
 import com.google.firebase.analytics.FirebaseAnalytics
-import stas.batura.di.ServiceLocator
 
 class AnaliticManagerImpl(context: Context) {
 
