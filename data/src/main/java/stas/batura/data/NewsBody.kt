bdc4c5bc6a057e2c0d88c3fe0ef6265cfe550808
Note <seeyou85@gmail.com>
2023-01-19 18:42:12 +0300

    pojo для новостей

--- data/src/main/java/stas/batura/data/NewsBody.kt
+++ data/src/main/java/stas/batura/data/NewsBody.kt
@@ -1,28 +1,19 @@
 package stas.batura.data
 
-import com.squareup.moshi.Json
-import stas.batura.retrofit.TimeLabel
-
 data class NewsBody(
     var title: String,  // заголовок поста
     var content:  String,  // дата-время поста в RFC3339
     var snippet: List<String>, // список категорий, массив строк
 
-    @Json(name = "image")
-    var imageUrl: String? = null,    // url картинки
+    var pic: String? = null,
 
-    @Json(name = "file_name")
-    var fileName:   String? = null,  // имя файла
+    var link:   String? = null,
 
-    @Json(name = "body")
-    var bodyHtml: String? = null,     // тело поста в HTML
+    var author: String? = null,
 
-    @Json(name = "show_notes")
-    var postText: String? = null,  // пост в текстовом виде
+    var geek: Boolean = false,
 
-    @Json(name = "audio_url")
-    var audioUrl: String? = null,   // url аудио файла
+    var votes: Int = 0,
 
-    @Json(name = "time_labels")
-    var timeLables: List<TimeLabel>? = null) // массив временых меток тем
+    var del: Boolean = false
 )
