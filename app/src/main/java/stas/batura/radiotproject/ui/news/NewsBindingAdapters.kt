0f3f3feb551e1b58cfb76d84dd3e257bc7c59d74
Note <seeyou85@gmail.com>
2023-01-23 00:47:31 +0300

    отображение сайта

--- app/src/main/java/stas/batura/radiotproject/ui/news/NewsBindingAdapters.kt
+++ app/src/main/java/stas/batura/radiotproject/ui/news/NewsBindingAdapters.kt
@@ -1,14 +1,41 @@
 package stas.batura.radiotproject.ui.news
 
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import stas.batura.data.NewsBody
+import timber.log.Timber
+import java.net.URI
 
 @BindingAdapter("newsTitleBind")
 fun TextView.newsTitleBind(newsBody: NewsBody) {
     text = "${newsBody.title}"
+}
+
+@BindingAdapter("newsSiteBind")
+fun TextView.newsSiteBind(newsBody: NewsBody) {
+    if (newsBody.link != null && newsBody.link != "") {
+        try {
+            val uri = URI(newsBody.link)
+            val domain: String = uri.host
+            val spannableString = SpannableString(domain)
+            spannableString.setSpan(
+                UnderlineSpan(),
+                0,
+                domain.length,
+                Spannable.SPAN_INCLUSIVE_INCLUSIVE
+            )
+            text = spannableString
+        } catch (e: java.lang.Exception) {
+            Timber.e(e.toString())
+        }
+    } else {
+        visibility = View.GONE
+    }
 }
 
 @BindingAdapter("newsBodyBind")
